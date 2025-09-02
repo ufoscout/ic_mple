f540b019499a1816fb8894605876e22d332cd40e
Francesco <ufoscout@gmail.com>
2025-09-02 11:47:31 +0200

    fix auth error serialization

--- src/ic_mple_auth/src/error.rs
+++ src/ic_mple_auth/src/error.rs
@@ -1,6 +1,8 @@
+use candid::CandidType;
+use serde::{Deserialize, Serialize};
 use thiserror::Error;
 
-#[derive(Debug, Error, PartialEq, Eq)]
+#[derive(Debug, Error, PartialEq, Eq, CandidType, Serialize, Deserialize, Clone)]
 pub enum AuthError {
     #[error("NotAuthorized")]
     NotAuthorized,
